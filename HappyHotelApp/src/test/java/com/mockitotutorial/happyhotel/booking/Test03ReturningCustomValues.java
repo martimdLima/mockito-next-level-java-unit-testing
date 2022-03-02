e33142a0a127300e40b125b7156c14ae9ea6544a
Martim Lima <martim.d.lima@protonmail.com>
2022-03-02 16:27:31 +0000

    Mockito 3 - basics; Mockito 3 - Advanced Concepts

---
+++ HappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test03ReturningCustomValues.java
@@ -0,0 +1,64 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
+
+
+public class Test03ReturningCustomValues {
+
+    private BookingService bookingService;
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setup() {
+
+        this.paymentServiceMock = mock(PaymentService.class);
+        this.roomServiceMock = mock(RoomService.class);
+        this.bookingDAOMock = mock(BookingDAO.class);
+        this.mailSenderMock = mock(MailSender.class);
+
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+   
+    }
+
+    @Test
+    void should_CountAvailablePlaces_When_OneRoomAvailable() {
+
+        // given
+        when(this.roomServiceMock.getAvailableRooms()).thenReturn(Collections.singletonList(new Room("Room 1", 2)));
+        int expected = 2;
+
+        // when
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_CountAvailablePlaces_When_MultipleRoomAvailable() {
+
+        // given
+        List<Room> roons = Arrays.asList(new Room("Room 1", 2), new Room("Room 2", 5), new Room("Room 3", 10));
+        when(this.roomServiceMock.getAvailableRooms()).thenReturn(roons);
+        int expected = 17;
+
+        // when
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
